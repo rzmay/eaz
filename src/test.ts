c9a9ae0ee3b91f0e8d7de9f281c78dc73ff66ec0
Robert May <robertmay@Roberts-MBP.fios-router.home>
2020-08-22 06:35:03 -0400

    Set up package for publish

--- src/test.ts
+++
@@ -1,7 +0,0 @@
-import { Easing } from './index';
-import DemoImage from './DemoImage';
-
-const cubic = Easing.cubic;
-const noise = Easing.noise(0.05, 10);
-
-DemoImage.interpolate(cubic, noise, 'src/images/cubicNoiseInterpolate.png')
---
+++ src/tests/test.ts
@@ -0,0 +1,7 @@
+import { Easing } from '../index';
+import DemoImage from '../DemoImage';
+
+const cubic = Easing.cubic;
+const noise = Easing.noise(0.05, 10);
+
+DemoImage.interpolate(cubic, noise, 'src/tests/images/cubicNoiseInterpolate.png')
